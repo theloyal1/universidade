ce616c0a789cc4903c7690659b56b66e5f743e85
theloyal1 <gabriel.leal.oliveira2004@gmail.com>
2023-12-02 14:33:48 -0300

    02/12/2023

--- src/main/java/formularios/FormCurso.java
+++ src/main/java/formularios/FormCurso.java
@@ -400,19 +400,19 @@ public class FormCurso extends javax.swing.JFrame {
             int res = JOptionPane.showConfirmDialog(this, "Confirmar exclusão?",
                     "Exclusão", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
             if (res == JOptionPane.YES_OPTION) {
-                Curso c = new Curso();
-                Iterator<Aluno> ia = c.getAlunos().iterator();
-                while(ia.hasNext()) {
-                    Aluno aux = (Aluno)ia.next();
-                    jcbAlunos.addItem(aux);
-                }
-                jtAlunos.removeAll();
-                Iterator<Professor> ip = c.getProfs().iterator();
-                while(ip.hasNext()) {
-                    Professor aux = (Professor)ip.next();
-                    jcbProfs.addItem(aux);
-                }
-                jtProfs.removeAll();
+//                Curso c = new Curso();
+//                Iterator<Aluno> ia = c.getAlunos().iterator();
+//                while(ia.hasNext()) {
+//                    Aluno aux = (Aluno)ia.next();
+//                    jcbAlunos.addItem(aux);
+//                }
+//                jtAlunos.removeAll();
+//                Iterator<Professor> ip = c.getProfs().iterator();
+//                while(ip.hasNext()) {
+//                    Professor aux = (Professor)ip.next();
+//                    jcbProfs.addItem(aux);
+//                }
+//                jtProfs.removeAll();
                 fichaCurso.excluir(jtCursos.getSelectedRow());
                 modeloCurso.removeRow(jtCursos.getSelectedRow());
                 JOptionPane.showMessageDialog(this, "Curso excluído com sucesso!");
